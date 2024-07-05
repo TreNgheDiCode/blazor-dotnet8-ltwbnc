e1d928951cdab9459f524043542fc8074a5ac930
Phuc140523 <117797349+Phuc140523@users.noreply.github.com>
2024-07-05 13:05:42 +0700

    gio hàng và update lại 1 số trang sản phẩm, update lại layout

--- ClientUser/Program.cs
+++ ClientUser/Program.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Syncfusion.Blazor;
 using Syncfusion.Blazor.Popups;
 using ClientUserLibrary.Helpers;
+using ClientUserLibrary.Services.CartService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -21,7 +22,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddTransient<CustomHttpHandler>();
 builder.Services.AddHttpClient("SystemApiClient", client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7016/products");
+    client.BaseAddress = new Uri("https://localhost:7016");
     client.DefaultRequestHeaders.Add("Access-Control-Allow-Origin", "*"); // Thêm header này n?u c?n thi?t
 }).AddHttpMessageHandler<CustomHttpHandler>();
 
@@ -33,6 +34,9 @@ builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStat
 builder.Services.AddScoped<IUserAccountService, UserAccountService>();
 builder.Services.AddScoped<IProductsService, ProductsService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<CartService>();
+
 
 // Syncfusion
 builder.Services.Configure<SyncfusionSection>(builder.Configuration.GetSection("Syncfusion"));
